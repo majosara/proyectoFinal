b430c55ac37ad6c6633e9e3779b3551636c9df72
majosara <38077766+majosara@users.noreply.github.com>
2018-04-28 08:25:53 -0500

    se crean blocs de notas y se crea la funcion de insertar pregunta

---
+++ insertar_pregunta.py
@@ -0,0 +1,4 @@
+def insertar_pregunta(pregunta, [respuestas], indice de respuesta correcta, materia, curso):
+    p = open('preguntas.txt')
+    o = open('opciones.txt')
+    r = open('respuesta_correcta')
