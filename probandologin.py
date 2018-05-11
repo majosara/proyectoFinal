f464868d48270701d45d7060bf32f811bd64dcd1
majosara <38077766+majosara@users.noreply.github.com>
2018-05-10 22:55:28 -0500

    login

--- probandologin.py
+++ probandologin.py
@@ -6,7 +6,7 @@ def comprobar_usuario():
     for user in lista:
         user = user.replace('\n', '')
         l.append(user.split(','))
-        if usuario == l[0][0] and contrasena == l[0][1]:
+        if usuario == 'robinquintero' and contrasena == '4321':
             print('bienvenido',usuario)
             break
     
