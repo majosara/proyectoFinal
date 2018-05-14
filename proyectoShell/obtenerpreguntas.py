b096223bc739eab4205633abb619bb43ea77aa17
majosara <38077766+majosara@users.noreply.github.com>
2018-05-13 22:58:00 -0500

    Proyecto en la shell
    funcion de obtener preguntas y respuestas

--- proyectoShell/obtenerpreguntas.py
+++ proyectoShell/obtenerpreguntas.py
@@ -1,4 +1,7 @@
 def getQuestions(grado):
+	"""
+
+	"""
 	preguntas = [] #lista que contiene las preguntas del juego
 	preguntas_grado = []
 	p = open('preguntas.txt','r')
@@ -12,8 +15,37 @@ def getQuestions(grado):
 	for preg in range(len(preguntas)):
 		if preguntas[preg][2] == str(grado):
 			preguntas_grado.append(preguntas[preg])
-	print(preguntas_grado)
+	
+	##print(preguntas_grado)
 
 if __name__ == '__main__':
 	getQuestions(1)
 
+def showQuestions(preguntas):
+	""" 
+	Esta funcion mostrara en pantalla las preguntas que se anadieron a la lista preguntas
+	"""
+	pass
+
+def getAnswers():
+	pass
+
+def getOptions(id_pregunta):
+	"""
+	Esta funcion Lista que contiene las opciones segun el id 
+	de cada pregunta
+	"""
+	options = []
+	id_options = []
+	opt = open('opciones.txt')
+
+	for opt in p:
+		opt = opt.replace('\n','')
+		options.append(opt.split(','))
+	opt.close()
+	options.pop(0)
+
+	for n in range(len(options)):
+		if options[n][0] == str(id_pregunta):
+			id_options.append(options[n])
+getOptions('00')
