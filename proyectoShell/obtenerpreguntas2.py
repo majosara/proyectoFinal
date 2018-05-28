3ba4aca71546a1bf3c0ae4db4c0ba4681d08ccb5
majosara <38077766+majosara@users.noreply.github.com>
2018-05-27 19:25:31 -0500

    creacion de nuevo archivo para el manejo de preguntas y mejora del codigo del login
    se revisa el codigo del login y las preguntas

---
+++ proyectoShell/obtenerpreguntas2.py
@@ -0,0 +1,102 @@
+id_answers = [] #esta variable contiene la respuesta correcta a una pregunta segun las opciones
+preguntas_grado = [] #esta variable contiene las preguntas segun el grado del usuario
+id_options = [] #esta variable contiene las opciones segun el id de la pregunta
+
+def getQuestions():
+	"""Esta funcion se utilizara para obtener las preguntas del
+	archivo plano y ponerlas en una lista"""
+	preguntas = []
+	p = open('preguntas.txt')
+
+	for pregunta in p:
+		pregunta = pregunta.replace('\n','')
+		preguntas.append(pregunta.split(','))
+
+	p.close()
+	preguntas.pop(0)
+	##print(preguntas)
+
+def getOptions(preguntas_grado,id_options):
+	"""Esta funcion se utilizara para obtener las opciones de las
+	preguntas que estan en un archivo plano y las pondra en una
+	lista"""
+	options = []
+	o = open('opciones.txt')
+
+	for opcion in o:
+		opcion = opcion.replace('\n','')
+		options.append(opcion.split(','))
+	o.close()
+	o.pop(0)
+
+	for i in range(len(options)):
+		if options[i][0] == srt(preguntas[0]):
+			id_options.append(options[i])
+
+def rightAnswers(preguntas_grado,id_answers):
+	"""Esta funcion se utilizara para obtener la respuesta correcta
+	de cada pregunta y las  pondra en una lista"""
+	
+	right_answers = [] #lista que contendra las respuestas correctas
+	ans = open('respuesta_correcta.txt')
+
+	for a in ans:
+		a = a.replace('\n','')
+		right_answers.append(a.split(','))
+	ans.close()
+	right_answers.pop(0)
+
+	for i in range(len(right_answers)):
+		if right_answers[i][1] == str(preguntas_grado[0]):
+			id_answers.append(right_answers[i])
+	#print(id_answers)
+
+
+def questions4grade(preguntas,grado,preguntas_grado):
+	"""Esta funcion pone en una lista las preguntas segun el usuario,
+	es decir, por el grado de dicho usuario"""
+	for preg in range(len(preguntas)):
+		if preguntas[preg][2] == str(grado):
+			preguntas_grado.append(preguntas[preg])
+			#print(preguntas_grado)
+
+def menuPreguntasOpciones(preguntas_grado,id_options,id_answers):
+	"""Esta funcion recibe las listas donde se encuentran las preguntas
+	segun el grado del usuario, las opciones y las respuestas e estas mismas"""
+	
+	for i in range(len(preguntas_grado)):
+		if preguntas_grado[i][0] == id_options[i][0]:
+			userAnswer = input("""{}
+				a.{}	b.{}
+				c.{}	d.{}
+				Respuesta: """.format(preguntas_grado[i][3], id_options[i][2], id_options[i][3],id_options[i][4],id_options[i][5]))
+			if userAnswer == id_answers[i][2]:
+				print('Correcto!')
+			else:
+				print('Incorrecto!')
+
+def showQuestions(usuario):
+	""" """
+	dataUser = [] #esta lista contiene la base de datos de los usuarios
+	infoUser = [] #esta lista contiene la informacion del usuario que se recibe como parametro
+
+	datos = open('datos_estudiantes.txt')
+
+	for user in datos:
+		user = user.replace('\n','')
+		dataUser.append(user.split(','))
+	datos.close()
+	dataUser.pop(0)
+
+	for i in range(len(dataUser)):
+		if dataUser[i][1] == usuario:
+			infoUser = dataUser[i]
+			print(infoUser)
+
+	questions4grade(preguntas,infoUser[6],preguntas_grado)
+
+	for i in range(len(preguntas_grado)):
+		getOptions(preguntas_grado[i][0],id_options)
+
+	for i in range(len(preguntas_grado)):
+		rightAnswers(preguntas_grado[i][0],id_answers)
--- proyectoShell/probandologin.py
+++ proyectoShell/probandologin.py
@@ -1,31 +1,51 @@
 from registrar_usuario import *
-from obtenerpreguntas import *
+from obtenerpreguntas2 import *
+
 def comprobar_usuario():
     rol = None
-    logged = False
-    lista = open('login.txt')
-    l = []
+    loggin = False
+    login = open('login.txt')
+    newlogin = [] #lista que contendra los datos de login.txt
     datos = []
     usuario = input('ingrese el usuario: ')
     contrasena = input('ingrese la contrasena: ')
-    for user in lista:
+    for user in login:
         user = user.replace('\n', '')
-        l.append(user.split(','))
+        newlogin.append(user.split(','))
 
     ##Verifica si el usuario se encuentra en la base de datos
-    for n in range(len(l)):
-        if usuario == l[n][0] and contrasena == l[n][1]:
-            logged = True
-            rol = l[n][0]
+    for i in range(len(newlogin)):
+        if usuario == newlogin[i][0] and contrasena == newlogin[i][1]:
+            loggin = True
+            rol = newlogin[i][0]
         else:
-            logged = False
-            
-        if logged:
+            loggin = False
+
+        if usuario not in newlogin[i][0]:
+            loggin = False
+            print('Usuario incorrecto o no registrado')
+            break
+        if contrasena not in newlogin[i][1]:
+            loggin=False
+            print('Contrasena incorrecta')
+            break
+
+            #if usuario != newlogin[i][0]:
+             #   loggin = False
+              #  print('Usuario incorrecto o no registrado')
+               # break
+            #if contrasena != newlogin[i][1]:
+             #   loggin = False
+              #  print('Contrasena incorrecta!')
+               # break
+
+        if loggin:
             if rol == 'admin':
                 registrar_usuario(datos)
             else:
-                print('Bienvenido a Check you!,',usuario)
-                return showQuestions(usuario)
+                print('Bienvenido a Check you!,',rol,usuario)
+                #return showQuestions(usuario)
 
-    
+
+
 comprobar_usuario()
